28f394f5bf52a9b1f606332ef23a1b76510560d4
lukaszkuczynski <kuczynskilukasz@gmail.com>
2025-08-12 18:14:12 +0200

    props in element

--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -2,10 +2,15 @@
 	let appName = 'BroadQuiz';
 	let url = 'https://jw.org';
 
+	let elements = ['Broadcast Sep 2025', 'Broadcast Oct 2025'];
+
 	import Likes from './Likes.svelte';
+	import QuizTiles from './QuizTiles.svelte';
 </script>
 
 <h1>Welcome to {appName}</h1>
 <p>Visit <a href={url}>this site</a> to watch and read really valuable stuff</p>
 
 <Likes />
+
+<QuizTiles {elements} />
---
+++ my-app/src/routes/QuizTiles.svelte
@@ -0,0 +1,10 @@
+<script>
+	let props = $props();
+</script>
+
+<h3>Choose your quiz</h3>
+<ul>
+	{#each props.elements as element}
+		<li>{element}</li>
+	{/each}
+</ul>
