b083621bd75f0ac5173b91eb647177341272677c
lukaszkuczynski <kuczynskilukasz@gmail.com>
2025-08-13 21:04:35 +0200

    some tests added

--- my-app/tests/quiz_prep.test.ts
+++ my-app/tests/quiz_prep.test.ts
@@ -1,3 +1,18 @@
+import { get_questions } from "$lib/quiz_prep"
+
 test('it is true', async () => {
     expect(true).toBeTruthy()
+})
+
+test("get questions returns list of them", async () => {
+    let questions = get_questions("quiz_name");
+    expect(questions).toHaveLength(2)
+})
+
+test("Question has all required parameters", async () => {
+    let questions = get_questions("some quiz")
+    let firstQuestion = questions[0]
+    expect(firstQuestion).toHaveProperty("type")
+    expect(firstQuestion).toHaveProperty("questionText")
+    expect(firstQuestion).toHaveProperty("correctAnswer")
 })
